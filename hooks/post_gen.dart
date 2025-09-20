0f52b98b060f63ac36ee9da0055adba2d5c089dd
HasanAlqaisi <hasan.alqaisi2000@gmail.com>
2025-09-20 14:15:28 +0300

    refactor: improve logging in post_gen.dart

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -5,7 +5,7 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Creating packages..');
   try {
-    await Process.run(
+    final packagesResult = await Process.run(
       'flutter',
       [
         'pub',
@@ -31,7 +31,15 @@ Future<void> run(HookContext context) async {
       workingDirectory: './${context.vars['projectName']}',
     );
 
-    await Process.run(
+    if (packagesResult.stderr != null && packagesResult.stderr!.isNotEmpty) {
+      context.logger.err('\nflutter pub add fails: ${packagesResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nPackages added successfully',
+      );
+    }
+
+    final devPackagesResult = await Process.run(
       'flutter',
       [
         'pub',
@@ -40,7 +48,7 @@ Future<void> run(HookContext context) async {
         'build_runner',
         'riverpod_generator',
         'custom_lint',
-        'riverpod_lint',
+        // 'riverpod_lint',
         'dart_mappable_builder',
         'auto_route_generator',
         'slang_build_runner',
@@ -50,40 +58,102 @@ Future<void> run(HookContext context) async {
       workingDirectory: './${context.vars['projectName']}',
     );
 
-    await Process.run(
+    if (devPackagesResult.stderr != null &&
+        devPackagesResult.stderr!.isNotEmpty) {
+      context.logger
+          .err('\nflutter pub add --dev fails: ${devPackagesResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nDev packages added successfully',
+      );
+    }
+
+    final getPackagesResult = await Process.run(
       'flutter',
       ['pub', 'get'],
       workingDirectory: './${context.vars['projectName']}',
     );
 
-    await Process.run(
+    if (getPackagesResult.stderr != null &&
+        getPackagesResult.stderr!.isNotEmpty) {
+      context.logger
+          .err('\n flutter pub get fails: ${getPackagesResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nPackages fetched successfully',
+      );
+    }
+
+    final packageRenameResult = await Process.run(
       'dart',
       ['run', 'package_rename'],
       workingDirectory: './${context.vars['projectName']}',
     );
 
-    await Process.run(
+    if (packageRenameResult.stderr != null &&
+        packageRenameResult.stderr!.isNotEmpty) {
+      context.logger.err(
+          '\n dart run package_rename fails: ${packageRenameResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nProject renamed to ${context.vars['packageName']} successfully',
+      );
+    }
+
+    final addLocalizationsResult = await Process.run(
       'flutter',
       ['pub', 'add', 'flutter_localizations', '--sdk=flutter'],
       workingDirectory: './${context.vars['projectName']}',
     );
 
-    await Process.run(
+    if (addLocalizationsResult.stderr != null &&
+        addLocalizationsResult.stderr!.isNotEmpty) {
+      context.logger.err(
+          '\n flutter pub add flutter_localizations fails: ${addLocalizationsResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nLocalizations added successfully',
+      );
+    }
+
+    final slangResult = await Process.run(
       'dart',
       ['run', 'slang'],
       workingDirectory: './${context.vars['projectName']}',
     );
+
+    if (slangResult.stderr != null && slangResult.stderr!.isNotEmpty) {
+      context.logger.err('\n dart run slang fails: ${slangResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nSlang ran successfully',
+      );
+    }
 
     // write to pubspec.yaml flutter_gen config
     final pubspec = File('./${context.vars['projectName']}/pubspec.yaml');
     pubspec.writeAsStringSync(pubspec.readAsStringSync() +
         '\n\nflutter_gen:\n  output: lib/generated/\n  line_length: 80\n\n  fonts:\n    outputs:\n      package_parameter_enabled: true\n\n  # Optional\n  integrations:\n    flutter_svg: true\n');
 
-    await Process.run(
+    final buildRunnerResult = await Process.run(
       'dart',
       ['run', 'build_runner', 'build'],
       workingDirectory: './${context.vars['projectName']}',
     );
+
+    if (buildRunnerResult.stderr != null &&
+        buildRunnerResult.stderr!.isNotEmpty) {
+      context.logger.err(
+          '\n dart run build_runner build fails: ${buildRunnerResult.stderr}');
+    } else {
+      context.logger.success(
+        '\nBuild runner ran successfully',
+      );
+    }
+
+    final widgetTest =
+        File('./${context.vars['projectName']}/test/widget_test.dart');
+    widgetTest.deleteSync();
 
     progress.complete('Your project is ready 🚀');
   } catch (e) {
